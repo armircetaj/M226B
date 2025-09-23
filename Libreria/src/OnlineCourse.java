5ffd241d85d6a89c4b8fba9486a3520ac6e7209a
armircetaj <pbh963@cpt.local>
2025-09-23 13:21:05 +0200

    Aggiunta Es Libreria

--- Libreria/src/OnlineCourse.java
+++ Libreria/src/OnlineCourse.java
@@ -1,7 +1,15 @@
-public class OnlineCourse {
+import java.util.Objects;
+
+public class OnlineCourse extends DigitalContent{
     private String instructor;
     private Difficulty difficulty;
     private int estimatedHours;
+    public OnlineCourse(String instructor, Difficulty difficulty, int estimatedHours, int id, String title, String author, double basePrice, double currentDiscount) {
+        super(id, title, author, basePrice, currentDiscount);
+        this.instructor = instructor;
+        this.difficulty = difficulty;
+        this.estimatedHours = estimatedHours;
+    }
     public boolean getCertificationEligible() {
         if (estimatedHours > 10) {
             return true;
@@ -10,4 +18,32 @@ public class OnlineCourse {
             return false;
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OnlineCourse that = (OnlineCourse) o;
+        return estimatedHours == that.estimatedHours && Objects.equals(instructor, that.instructor) && difficulty == that.difficulty;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instructor, difficulty, estimatedHours);
+    }
+
+    @Override
+    public String getContentType() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public void applyDiscount(double percentage) {
+
+    }
 }
+
